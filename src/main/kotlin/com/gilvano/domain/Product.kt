77f48875b76d161614a1123235e7e22d0e678c20
Gilvano Piontkoski <g.piontkoski@gmail.com>
2022-02-23 14:23:32 -0300

    Criar domain produto

---
+++ src/main/kotlin/com/gilvano/domain/Product.kt
@@ -0,0 +1,17 @@
+package com.gilvano.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Product (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    val name: String,
+    val price: Double,
+    val quantityInStock: Int
+)
