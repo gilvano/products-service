1dccd466789d19ef8240c54ebfbfa6e22be565e5
Gilvano Piontkoski <g.piontkoski@gmail.com>
2022-02-24 16:51:13 -0300

    Implentar testes do find by id

--- src/test/kotlin/com/gilvano/services/impl/ProductServiceImplTest.kt
+++ src/test/kotlin/com/gilvano/services/impl/ProductServiceImplTest.kt
@@ -3,12 +3,14 @@ package com.gilvano.services.impl
 import com.gilvano.domain.Product
 import com.gilvano.dto.ProductRequest
 import com.gilvano.exceptions.AlreadyExistsException
+import com.gilvano.exceptions.ProductNotFoundException
 import com.gilvano.repository.ProductRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrowsExactly
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import java.util.*
 
 internal class ProductServiceImplTest {
     private val productRepository = Mockito.mock(ProductRepository::class.java)
@@ -43,6 +45,29 @@ internal class ProductServiceImplTest {
 
         assertThrowsExactly(AlreadyExistsException::class.java) {
             productService.create(productRequest)
+        }
+    }
+
+    @Test
+    fun `when findById method is call with valid id a Product is returned`() {
+        val productInput = 1L
+        val productOutput = Product(id = 1, name = "Product Name", price = 10.00, quantityInStock = 5)
+
+        `when`(productRepository.findById(productInput))
+            .thenReturn(Optional.of(productOutput))
+
+
+        val productResponse = productService.findById(productInput)
+
+        assertEquals(productInput, productResponse.id)
+        assertEquals(productOutput.name, productResponse.name)
+    }
+
+    @Test
+    fun `when findById method is call with invalid id, throws ProductNotFoundException`() {
+        val id = 1L
+        assertThrowsExactly(ProductNotFoundException::class.java) {
+            productService.findById(id)
         }
     }
 }
