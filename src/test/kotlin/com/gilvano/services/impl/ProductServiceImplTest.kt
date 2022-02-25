3b83fe3a9c1bf6d3998ee643e5552ff54e8cd58e
Gilvano Piontkoski <g.piontkoski@gmail.com>
2022-02-25 11:30:48 -0300

    Implementar testes para metodo update

--- src/test/kotlin/com/gilvano/services/impl/ProductServiceImplTest.kt
+++ src/test/kotlin/com/gilvano/services/impl/ProductServiceImplTest.kt
@@ -2,6 +2,7 @@ package com.gilvano.services.impl
 
 import com.gilvano.domain.Product
 import com.gilvano.dto.ProductRequest
+import com.gilvano.dto.ProductUpdateRequest
 import com.gilvano.exceptions.AlreadyExistsException
 import com.gilvano.exceptions.ProductNotFoundException
 import com.gilvano.repository.ProductRepository
@@ -69,5 +70,47 @@ internal class ProductServiceImplTest {
         assertThrowsExactly(ProductNotFoundException::class.java) {
             productService.findById(id)
         }
+    }
+
+    @Test
+    fun `when update method is call with duplicated product name, throws AlreadyExistsException`() {
+        val productInput = Product(id = null, name = "Product Name", price = 10.00, quantityInStock = 5)
+        val productOutput = Product(id = 1, name = "Product Name", price = 10.00, quantityInStock = 5)
+
+
+        `when`(productRepository.findByNameIgnoreCase(productInput.name))
+            .thenReturn(productOutput)
+
+        val productRequest = ProductUpdateRequest(id = 1, name = "Product Name", price = 10.00, quantityInStock = 5)
+
+        assertThrowsExactly(AlreadyExistsException::class.java) {
+            productService.update(productRequest)
+        }
+    }
+
+    @Test
+    fun `when update method is call with invalid id, throws ProductNotFoundException`() {
+        val productRequest = ProductUpdateRequest(id = 2, name = "Product Name", price = 10.00, quantityInStock = 5)
+        assertThrowsExactly(ProductNotFoundException::class.java) {
+            productService.update(productRequest)
+        }
+    }
+
+    @Test
+    fun `when update method is call with valid data a Product is returned`() {
+        val productOriginal = Product(id = 1, name = "Product Name", price = 10.00, quantityInStock = 5)
+        val productUpdated = Product(id = 1, name = "updated product", price = 11.00, quantityInStock = 5)
+
+
+        `when`(productRepository.findById(productOriginal.id!!))
+            .thenReturn(Optional.of(productOriginal))
+        `when`(productRepository.update(productUpdated))
+            .thenReturn(productUpdated)
+
+        val productRequest = ProductUpdateRequest(id = 1, name = "updated product", price = 11.00, quantityInStock = 5)
+
+        val productResponse = productService.update(productRequest)
+
+        assertEquals(productRequest.name, productResponse.name)
     }
 }
