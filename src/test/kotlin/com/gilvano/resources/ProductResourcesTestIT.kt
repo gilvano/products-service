4c8f4f1607b00e6c31187b02e9a5b1fc404f2412
Gilvano Piontkoski <g.piontkoski@gmail.com>
2022-02-25 09:28:54 -0300

    Implementar testes de integração para as exceções

--- src/test/kotlin/com/gilvano/resources/ProductResourcesTestIT.kt
+++ src/test/kotlin/com/gilvano/resources/ProductResourcesTestIT.kt
@@ -3,8 +3,11 @@ package com.gilvano.resources
 import com.gilvano.FindByIdServiceRequest
 import com.gilvano.ProductServiceRequest
 import com.gilvano.ProductsServiceGrpc.ProductsServiceBlockingStub
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 @MicronautTest
@@ -27,6 +30,25 @@ internal class ProductResourcesTestIT(
     }
 
     @Test
+    fun `when ProductsServiceGrpc create method is call with invalid data a AlreadyExistsException is returned`() {
+        val request = ProductServiceRequest.newBuilder()
+            .setName("product 1")
+            .setPrice(20.99)
+            .setQuantityInStock(10)
+            .build()
+
+        val description = "Produto ${request.name} já cadastrado no sistema."
+
+
+        val response = assertThrows(StatusRuntimeException::class.java) {
+            productsServiceBlockingStub.create(request)
+        }
+
+        Assertions.assertEquals(Status.ALREADY_EXISTS.code, response.status.code)
+        Assertions.assertEquals(description, response.status.description)
+    }
+
+    @Test
     fun `when ProductsServiceGrpc findById method is call with valid id a success is returned`() {
         val request = FindByIdServiceRequest.newBuilder()
             .setId(1)
@@ -36,5 +58,21 @@ internal class ProductResourcesTestIT(
 
         Assertions.assertEquals(1, response.id)
         Assertions.assertEquals("product 1", response.name)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc findById method is call with invalid id a ProductNotFound is returned`() {
+        val request = FindByIdServiceRequest.newBuilder()
+            .setId(10)
+            .build()
+
+        val description = "Produto com id ${request.id} não encontrado"
+
+        val response = assertThrows(StatusRuntimeException::class.java) {
+            productsServiceBlockingStub.findById(request)
+        }
+
+        Assertions.assertEquals(Status.NOT_FOUND.code, response.status.code)
+        Assertions.assertEquals(description, response.status.description)
     }
 }
